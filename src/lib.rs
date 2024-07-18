44e1bbcf7d836ebcaa59bfcf5401c0cdbb61aad8
xuxiaocheng <2762267080@qq.com>
2024-07-18 09:40:27 +0800

    refactor(lib): 统一lock

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 #![doc = include_str!("../README.md")]
-#[forbid(unsafe_code)]
-#[warn(missing_docs)]
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
+
 use std::future::Future;
 use std::pin::Pin;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard};
 use std::task::{Context, Poll, Waker};
 
 use pin_project_lite::pin_project;
@@ -14,29 +15,6 @@ pin_project! {
         #[pin]
         future: F,
         inner: Controller,
-    }
-}
-
-#[derive(Debug, Clone)]
-/// The controller of a `Pausable` future.
-pub struct Controller(Arc<Mutex<ControllerInner>>);
-
-#[derive(Debug)]
-struct ControllerInner {
-    paused: bool,
-    cx: Option<Waker>,
-}
-
-impl<F: Future> Future for Pausable<F> {
-    type Output = F::Output;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let me = self.project();
-        let mut inner = (*me.inner).0.lock().unwrap_or_else(|e| e.into_inner());
-        if !inner.paused { return me.future.poll(cx); }
-        let cx = cx.waker().clone();
-        inner.cx.replace(cx);
-        Poll::Pending
     }
 }
 
@@ -58,22 +36,36 @@ impl<F> Pausable<F> {
     }
 }
 
+#[derive(Debug, Clone)]
+/// The controller of a `Pausable` future.
+pub struct Controller(Arc<Mutex<ControllerInner>>);
+
+#[derive(Debug)]
+struct ControllerInner {
+    paused: bool,
+    cx: Option<Waker>,
+}
+
 impl Controller {
+    fn inner(&self) -> MutexGuard<ControllerInner> {
+        self.0.lock().unwrap_or_else(|e| e.into_inner())
+    }
+
     /// Is the future paused?
     pub fn is_paused(&self) -> bool {
-        let me = self.0.lock().unwrap_or_else(|e| e.into_inner());
+        let me = self.inner();
         me.paused
     }
 
     /// Pause the future.
     pub fn pause(&self) {
-        let mut me = self.0.lock().unwrap_or_else(|e| e.into_inner());
+        let mut me = self.inner();
         me.paused = true;
     }
 
     /// Resume the future.
     pub fn resume(&self) {
-        let mut me = self.0.lock().unwrap_or_else(|e| e.into_inner());
+        let mut me = self.inner();
         me.paused = false;
         if let Some(cx) = me.cx.take() {
             cx.wake();
@@ -81,22 +73,29 @@ impl Controller {
     }
 }
 
+impl<F: Future> Future for Pausable<F> {
+    type Output = F::Output;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let me = self.project();
+        let mut inner = (*me.inner).0.lock().unwrap_or_else(|e| e.into_inner());
+        if !inner.paused { return me.future.poll(cx); }
+        let cx = cx.waker().clone();
+        inner.cx.replace(cx);
+        Poll::Pending
+    }
+}
+
 #[cfg(feature = "stream")]
-mod __stream {
-    use futures::Stream;
+impl<S: futures::Stream> futures::Stream for Pausable<S> {
+    type Item = S::Item;
 
-    use super::*;
-
-    impl<S: Stream> Stream for Pausable<S> {
-        type Item = S::Item;
-
-        fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-            let me = self.project();
-            let mut inner = (*me.inner).0.lock().unwrap_or_else(|e| e.into_inner());
-            if !inner.paused { return me.future.poll_next(cx); }
-            let cx = cx.waker().clone();
-            inner.cx.replace(cx);
-            Poll::Pending
-        }
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let me = self.project();
+        let mut inner = (*me.inner).inner();
+        if !inner.paused { return me.future.poll_next(cx); }
+        let cx = cx.waker().clone();
+        inner.cx.replace(cx);
+        Poll::Pending
     }
 }
